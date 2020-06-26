ed6ce8e256107bc7320cfa28b77106dd61e51238
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-06-26 20:42:47 +0200

    add class declaration

--- class.js
+++ class.js
@@ -7,3 +7,22 @@
 // - instroduced in ES6
 // - syntactical sugar over prototype-based inheritance
 
+// 1. Class declaration
+class Person {
+	// constructor
+	constructor(name, age) {
+		// fields
+		this.name = name;
+		this.age = age;
+	}
+	//methods
+	speak() {
+		console.log(`${this.name}: hello!`);
+	}
+}
+
+const dasom = new Person("dasom", 27);
+console.log(dasom);
+console.log(dasom.name);
+console.log(dasom.age);
+dasom.speak();
