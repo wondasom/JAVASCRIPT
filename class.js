2c8a3105942f108d6c5e907aa27c2b34db783044
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-06-26 20:47:12 +0200

    add public, private fields and static properties, methods

--- class.js
+++ class.js
@@ -27,7 +27,6 @@ console.log(dasom.name);
 console.log(dasom.age);
 dasom.speak();
 
-
 // 2. Getter and Setter
 class User {
 	constructor(firstName, lastName, age) {
@@ -61,3 +60,30 @@ class User {
 const user1 = new User("Steve", "Job", -1);
 console.log(user1.age);
 
+// 3. Fields (public, private)
+// Too soon!
+class Experiment {
+	publicField = 2;
+	#privateField = 0;
+}
+
+const experiment = new Experiment();
+console.log(experiment.publicField);
+console.log(experiment.privateField);
+
+// 4. Static properties and methods
+// Too soon!
+class Article {
+	static publisher = "Dasom Won";
+	constructor(articleNumber) {
+		this.articleNumber = articleNumber;
+	}
+	static printPublisher() {
+		console.log(Article.publisher);
+	}
+}
+const article1 = new Article(1);
+const article2 = new Article(2);
+console.log(article1.publisher); // undefined -> because publisher is static
+console.log(Article.publisher); // 'Dasom Won'
+Article.printPublisher(); // 'Dasom Won'
