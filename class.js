b9154700c2ce7d63810ddf380981d85981dc98fa
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-06-26 20:42:03 +0200

    create class document

---
+++ class.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// Object-oriented programming
+// class: template (ex - 붕어빵 틀)
+// object: instance of a class (ex - 팥붕어빵, 슈크림붕어빵)
+// JavaScript classes
+// - instroduced in ES6
+// - syntactical sugar over prototype-based inheritance
+
