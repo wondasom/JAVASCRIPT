28008d508b30c60dbe924e52c3f63d4ed761d539
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-06-29 13:46:38 +0200

    3. Looping over an array

--- array.js
+++ array.js
@@ -13,3 +13,20 @@ console.log(fruits.length); // 2
 console.log(fruits[0]); // 🍎
 console.log(fruits[1]); // 🍌
 console.log(fruits[fruits.length - 1]); // 🍌
+
+console.clear();
+
+// 3. Looping over an array
+// print all fruits
+// (a) for
+for (let i = 0; i < fruits.length; i++) {
+	console.log(`(a)` + fruits[i]);
+}
+
+//(b) for of
+for (let fruit of fruits) {
+	console.log(`(b)` + fruit);
+}
+
+// (c) forEach
+fruits.forEach((fruit) => console.log("(c)" + fruit));
