8144d0a47cbfd91bd94f46a03c60f5be5d6ed3c3
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-06-29 13:36:49 +0200

    2. Index position

--- array.js
+++ array.js
@@ -5,3 +5,11 @@
 // 1. Declaration
 const arr1 = new Array();
 const arr2 = [1, 2];
+
+// 2. Index position
+const fruits = ["🍎", "🍌"];
+console.log(fruits);
+console.log(fruits.length); // 2
+console.log(fruits[0]); // 🍎
+console.log(fruits[1]); // 🍌
+console.log(fruits[fruits.length - 1]); // 🍌
