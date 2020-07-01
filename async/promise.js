4d602f85c13ae2f1d69b4ac82735f9047348dca6
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-01 15:41:11 +0200

    PROMISE 1. Producer

---
+++ async/promise.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Promise is a JavaScript object for asynchronous operation.
+
+// 📖 Study Point
+// ㄴ 1. State: pending -> fulfilled or rejected
+// ㄴ 2. Producer vs Consumer
+
+// 1. Producer
+// ❗️when a new Promise is created, the executor runs automatically.
+const promise = new Promise((resolve, reject) => {
+	// doing some heavy work (network, read files)
+	console.log("doing something");
+	setTimeout(() => {
+		resolve("dasom");
+		reject(new Error("no network"));
+	}, 2000);
+});
