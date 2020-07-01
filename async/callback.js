d15058b3bb5ab2127655b5b91ed89bab902e1cf9
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-01 14:09:52 +0200

    CALLBACK example of callback hell

--- async/callback.js
+++ async/callback.js
@@ -21,3 +21,60 @@ function printWithDelay(print, timeout) {
 	setTimeout(print, timeout);
 }
 printWithDelay(() => console.log("async callback"), 2000); // async
+
+
+
+// Callback Hell example
+// 1. it is not common to request Backend to have user's role(guest, admin etc).
+// it usually comes together with login.
+// however, we have this as an exception for a example's sake.
+// 2. since we don't have a backend now,
+// we'll use setTimeout instead, to pretend we have a server.
+class UserStorage {
+	loginUser(id, password, onSuccess, onError) {
+		setTimeout(() => {
+			if (
+				(id === "ellie" && password === "amazingTeacher") ||
+				(id === "dasom" && password === "coder")
+			) {
+				onSuccess(id);
+			} else {
+				onError(new Error("not Found"));
+			}
+		}, 2000);
+	}
+	getRoles(user, onSuccess, onError) {
+		setTimeout(() => {
+			if (user === "ellie") {
+				onSuccess({ name: "ellie", role: "admin" });
+			} else {
+				onError(new Error("no access"));
+			}
+		}, 1000);
+	}
+}
+
+const userStorage = new UserStorage();
+const id = prompt("enter your id");
+const password = prompt("enter your password");
+
+userStorage.loginUser(
+	id,
+	password,
+	(user) => {
+		userStorage.getRoles(
+			user,
+			(userWithRole) => {
+				alert(
+					`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`
+				);
+			},
+			(error) => {
+				console.log(error);
+			}
+		);
+	},
+	(error) => {
+		console.log(error);
+	}
+);
