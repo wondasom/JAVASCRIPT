269f3aa97e8a16f41e342c285fa6850b1e9bdb57
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-01 13:45:22 +0200

    CALLBACK 1. Synchronous callback

---
+++ async/callback.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// JavaScript is synchronous
+// Execute the code block in order after hoisting
+// *hoisting: putting function/variable declarations into memory BEFORE it executes any code segment
+
+console.log(`1`); // sync
+setTimeout(() => console.log("2"), 1000);
+// setTimeout is a browser API -> a request to a browser, async
+console.log(`3`); // sync
+// result -> 1 3 2
+
+// 1. Synchronous callback
+function printImmediately(print) {
+	print();
+}
+printImmediately(() => console.log("hello")); // sync
+
