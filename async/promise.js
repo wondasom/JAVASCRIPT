294b69fa91d1dc08a30845fc1323a2594294e3ea
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-01 15:43:32 +0200

    PROMISE 3. Promise chanining

--- async/promise.js
+++ async/promise.js
@@ -30,3 +30,18 @@ promise
 		console.log("finally");
 	});
 
+// 3. Promise chaining
+const fetchNumber = new Promise((resolve, reject) => {
+	setTimeout(() => resolve(1), 1000);
+});
+
+fetchNumber
+	.then((num) => num * 2)
+	.then((num) => num * 3)
+	.then((num) => {
+		return new Promise((resolve, reject) => {
+			setTimeout(() => resolve(num - 1), 1000);
+		});
+	})
+	.then((num) => console.log(num));
+
