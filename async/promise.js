81cefa973623f352d73d4ad9765bfb4e5aafe305
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-01 15:44:36 +0200

    PROMISE 4. Error Handling

--- async/promise.js
+++ async/promise.js
@@ -45,3 +45,34 @@ fetchNumber
 	})
 	.then((num) => console.log(num));
 
+// 4. Error Handling
+const getHen = () =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => resolve("🐓"), 1000);
+	});
+const getEgg = (hen) =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => {
+			// resolve(`${hen} => 🥚`);
+			reject(new Error(`error! ${hen} => 🥚`));
+		}, 1000);
+	});
+const cook = (egg) =>
+	new Promise((resolve, reject) => {
+		setTimeout(() => resolve(`${egg} => 🍳`), 1000);
+	});
+
+// getHen()
+// 	.then((hen) => getEgg(hen))
+// 	.then((egg) => cook(egg))
+// 	.then((meal) => console.log(meal));
+// This can be shortened like below
+
+getHen()
+	.then(getEgg)
+	.catch((error) => {
+		return "🥖";
+	})
+	.then(cook)
+	.then(console.log)
+	.catch(console.log);
