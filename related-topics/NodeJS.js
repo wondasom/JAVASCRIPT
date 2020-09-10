83e6fe9106b06aa4cccda70ef88502c5536fb99d
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-09-10 18:27:38 +0200

    craete a new folder and a file for NodeJS

---
+++ related-topics/NodeJS.js
@@ -0,0 +1,9 @@
+// What is NodeJS?
+
+// what do you do with it
+// 1. to build utilities on your machine (front-end developer)
+// ㄴ the V8 runs on your machine so that you can opne your js files locally, listen to HTTP request, access databases directly etc
+// 2. a web server(express, koa) 
+
+
+
