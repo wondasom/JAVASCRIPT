0797c90c3c32a8ef79a2a591cf346d461313e7cb
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-09-10 18:34:52 +0200

    NODEJS: add contents to the file

--- related-topics/NodeJS.js
+++ related-topics/NodeJS.js
@@ -1,9 +1,13 @@
 // What is NodeJS?
-
-// what do you do with it
-// 1. to build utilities on your machine (front-end developer)
+// 1. A runtime environment for executing JavaScript code(outside of the browser)
+// ㄴ to build utilities on your machine (front-end developer)
 // ㄴ the V8 runs on your machine so that you can opne your js files locally, listen to HTTP request, access databases directly etc
-// 2. a web server(express, koa) 
+// 2. NodeJS is also often used to build back-end services
+// ㄴ a web server(express, koa) 
+// ㄴ to create API(Application Programming Interface)
+// ㄴ for highly-scalable, data-intensive and real-time apps 
+// ㄴ great for prototyping
+
 
 
 
